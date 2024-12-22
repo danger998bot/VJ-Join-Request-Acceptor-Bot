d2d7cce702d007ede4a785952a3606a4232c48f4
danger998bot <sibasahu036@gmail.com>
2024-12-22 21:14:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "22336285"))
+API_HASH = environ.get("API_HASH", "3d84051d72e78013f6b38a1b7e26e469")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002322460153"))
+ADMINS = int(environ.get("ADMINS", "7547798233"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://sibasahu036:f28evZw6b5K0JfeK@cluster0.7qoey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
